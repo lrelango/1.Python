71f834ba09835719912e2b57216320375a9d5715
lrelango <lrelango@gmail.com>
2025-06-11 10:58:08 +0530

    Add files via upload

---
+++ 1-python.py
@@ -0,0 +1,30 @@
+print("HOPE AI")
+instituteName = input("Enter your institute name?")
+print (instituteName)
+
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+school = input("Enter your school name:")
+degree = input("Enter your drgree:")
+
+print("My name is : ", name)
+print("My age is : ", age)
+print("My school name is : ", school)
+print("My degree is : ", degree)
+
+a= 43
+b= 34
+add = a + b
+print(add)
+print(a-b)
+print(a*b)
+print(a/b)
+
+a=76
+b=31
+print(a//b)
+print(a%b)
+
+a=4
+b=4
+print(a**b)
